2967c488ef472d8e834819f97fd1616b99e3983b
John Colby <john.b.colby@gmail.com>
2019-10-22 00:37:57 -0700

    Add ‘pretrained’ as an input argument

--- rsna_heme/cnn.py
+++ rsna_heme/cnn.py
@@ -2,7 +2,7 @@ import gluoncv
 import mxnet as mx
 
 def get_model(args):
-    net = gluoncv.model_zoo.get_model(args.model_name, pretrained = False)
+    net = gluoncv.model_zoo.get_model(args.model_name, pretrained = args.pretrained)
     with net.name_scope():
         if hasattr(net, 'fc'):
             output_name = 'fc'
@@ -11,6 +11,8 @@ def get_model(args):
         setattr(net, output_name, mx.gluon.nn.Dense(args.classes))
     if hasattr(args, 'params_path'):
         net.load_parameters(args.params_path)
+    elif args.pretrained is True:
+        getattr(net, output_name).initialize(mx.init.Xavier(), ctx = args.ctx)
     else:
         net.initialize(mx.init.Xavier(), ctx = args.ctx)
     net.collect_params().reset_ctx(args.ctx)
